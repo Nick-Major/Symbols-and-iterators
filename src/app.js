b5396713f3ce0c2e64e50021a3d10e4679eb795c
Nick-Major <divanchik26@gmail.com>
2024-11-27 17:23:54 +0300

    Logic update

--- src/app.js
+++ src/app.js
@@ -4,13 +4,32 @@ import { swordsman } from "./swordsman.js";
 
 class Team {
     constructor(...characters) {
-        this.arrOfCharacters = [...characters];
+        this.listOfCharacters = {
+            characters: [...characters],
+            [Symbol.iterator]() {
+                let index = 0;
+                return {
+                    next: () => {
+                        if (index < this.characters.length) {
+                            return {
+                                value: this.characters[index++],
+                                done: false
+                            };
+                        } else {
+                            return {value: undefined, done: true};
+                        }
+                    }
+                };
+            }
+        };
     }
-}
+};
+
+
 
 let team = new Team(bowerman, daemon, swordsman);
 
-for(const char of team) {
+for (let char of team.listOfCharacters) {
     console.log(char);
+    
 }
-
